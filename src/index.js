608736419c528179dedd762fa6c0d242091885cd
Salvador Ariza <sariza@thoughtworks.com>
2017-11-28 12:51:07 -0600

    extracted App into its own component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Header from './components/Header';
-
-const App = () => {
-  return (
-    <div>
-      <Header message='Naming Contest'/>
-      <div>
-
-      </div>
-    </div>
-  );
-};
+import App from './components/App';
 
 render(<App/>, document.getElementById('root'));
